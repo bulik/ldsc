3d0c4464777b2578bf6f13386f0c1c9ab7d55046
Hervé Ménager <herve.menager@gmail.com>
2020-02-16 22:03:47 +0100

    add a setup script and requirements file for install (#176)
    * add a setup script and requirements file for install
    
    * add version ranges for reqs in setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='ldsc',
+      version='1.0',
+      description='LD Score Regression (LDSC)',
+      url='http://github.com/bulik/ldsc',
+      author='Brendan Bulik-Sullivan and Hilary Finucane',
+      author_email='',
+      license='GPLv3',
+      packages=['ldscore'],
+      scripts=['ldsc.py', 'munge_sumstats.py'],
+      install_requires = [
+            'bitarray>=0.8,<0.9',
+            'nose>=1.3,<1.4',
+            'pybedtools>=0.7,<0.8',
+            'scipy>=0.18,<0.19',
+            'numpy>=1.10,<1.11',
+            'pandas>=0.20,<0.21'
+      ]
+)
