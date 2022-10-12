cbe8fcfb457efeee192f6d8c875d663b65d868c7
Alex Petty <pettyalex@gmail.com>
2022-10-12 15:13:16 -0500

    Add readme.md into the setup.py for packaging into PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='ldsc',
-      version='2.0',
+      version='2.0.0',
       description='LD Score Regression (LDSC)',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/bulik/ldsc',
       author='Brendan Bulik-Sullivan and Hilary Finucane',
       author_email='',
