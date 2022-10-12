9b3e1d5e75301a47a663f9074490d6c749ef8945
Alex Petty <pettyalex@gmail.com>
2022-10-12 15:53:26 -0500

    do not pin to specific versions in install_requires to allower newer dependencies

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(name='ldsc',
       packages=['ldscore'],
       scripts=['ldsc.py', 'munge_sumstats.py', 'make_annot.py'],
       install_requires = [
-            'bitarray==2.6.0',
-            'nose==1.3.7', # this is only a test dep
-            'pybedtools==0.9.0',
-            'scipy==1.9.2',
-            'numpy==1.23.3',
-            'pandas==1.5.0'
+            'bitarray>=2.6.0',
+            'nose>=1.3.7', # this is only a test dep
+            'pybedtools>=0.9.0',
+            'scipy>=1.9.2',
+            'numpy>=1.23.3',
+            'pandas>=1.5.0'
       ]
 )
